829558046b96492ce5ebb571b4a1a89b38964b77
Alex <du.alexcruz@gmail.com>
2019-02-01 16:55:15 -0600

    Battery temp: Use MCC to determine the scale type

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -134,8 +134,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     BatteryTipPreferenceController mBatteryTipPreferenceController;
     private int mStatsType = BatteryStats.STATS_SINCE_CHARGED;
 
-    private boolean batteryTemp = false;
-
     @VisibleForTesting
     LoaderManager.LoaderCallbacks<BatteryInfo> mBatteryInfoLoaderCallbacks =
             new LoaderManager.LoaderCallbacks<BatteryInfo>() {
@@ -246,7 +244,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         restartBatteryInfoLoader();
         mBatteryTipPreferenceController.restoreInstanceState(icicle);
         updateBatteryTipFlag(icicle);
-        updateBatteryTempPreference();
     }
 
     @Override
@@ -265,8 +262,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                         .setTitle(R.string.advanced_battery_title)
                         .launch();
             return true;
-        } else if (KEY_BATTERY_TEMP.equals(preference.getKey())) {
-            updateBatteryTempPreference();
         }
         return super.onPreferenceTreeClick(preference);
     }
@@ -387,9 +382,12 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
         updateLastFullChargePreference();
-        updateBatteryTempPreference();
         mScreenUsagePref.setSubtitle(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
+        mBatteryTemp.setSubtitle(
+                com.android.internal.util.havoc.HavocUtils.mccCheck(getContext()) ?
+                com.android.internal.util.havoc.HavocUtils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.havoc.HavocUtils.batteryTemperature(getContext(), false) + "°C");
 
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,
@@ -430,19 +428,6 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
             mLastFullChargePref.setSummary(
                     StringUtil.formatRelativeTime(getContext(), lastFullChargeTime,
                             false /* withSeconds */));
-        }
-    }
-
-    @VisibleForTesting
-    void updateBatteryTempPreference() {
-        if (batteryTemp) {
-            mBatteryTemp.setSubtitle(
-                com.android.internal.util.havoc.HavocUtils.batteryTemperature(getContext(), false));
-            batteryTemp = false;
-        } else {
-            mBatteryTemp.setSubtitle(
-                com.android.internal.util.havoc.HavocUtils.batteryTemperature(getContext(), true));
-            batteryTemp = true;
         }
     }
 
